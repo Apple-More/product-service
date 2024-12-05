133736340fc0bdfe66f5593ef39e1555cca49bc2
Haritha Weerathunga <122088594+harithaw@users.noreply.github.com>
2024-12-05 11:41:35 +0530

    updated azure blob secrets

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,6 @@ export const PORT = process.env.PORT;
 export const USER_SERVICE_URL = process.env.USER_SERVICE_URL;
 export const ORDER_SERVICE_URL = process.env.ORDER_SERVICE_URL;
 export const PRODUCT_SERVICE_URL = process.env.PRODUCT_SERVICE_URL;
-export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET as string;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const AZURE_BLOB_CONNECTION_STRING = process.env.AZURE_BLOB_CONNECTION_STRING;
+export const AZURE_BLOB_CONTAINER_NAME = process.env.AZURE_BLOB_CONTAINER_NAME;
